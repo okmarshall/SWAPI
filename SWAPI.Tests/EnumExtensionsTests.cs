b718a1193399c8a57dc77540694e6459431b72bc
okmarshall <oliverkmarshall@gmail.com>
2019-09-06 20:24:39 +0100

    Added test project with extension method test

---
+++ SWAPI.Tests/EnumExtensionsTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using SWAPI.Library.Enums;
+
+namespace SWAPI.Tests
+{
+    [TestFixture]
+    public class EnumExtensionsTests
+    {
+        [Test]
+        public void When_Extension_Method_Called_Then_Correct_Resource_Names_Are_Returned()
+        {
+            var resources = new Resource[] { Resource.People, Resource.Films, Resource.Starships, Resource.Vehicles, Resource.Species, Resource.Planets };
+
+            foreach (var resource in resources)
+            {
+                var expected = resource.ToString().ToLower();
+                var actual = resource.GetName();
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
