043d17d754b54d9c60bf316b6c2a38451a62a910
okmarshall <oliverkmarshall@gmail.com>
2019-09-06 18:36:37 +0100

    POC - Make a request to the API

--- SWAPI.Console/Program.cs
+++ SWAPI.Console/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Threading.Tasks;
+using SWAPI.Library;
 
 namespace SWAPI.UI
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var requestManager = new RequestManager();
+
+            var result = await requestManager.MakeRequest();
+
+            Console.WriteLine(result);
         }
     }
 }
---
+++ SWAPI.Library/RequestManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SWAPI.Library
+{
+    public class RequestManager
+    {
+        public async Task<string> MakeRequest()
+        {
+            var httpClient = new HttpClient();
+
+            httpClient.BaseAddress = new Uri("https://swapi.co/api/");
+
+            var response = await httpClient.GetAsync("planets/1");
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
